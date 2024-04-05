070824338b391a3be02bd0ccc4763f042faf5019
bello <tkdgns0470@gmail.com>
2024-04-05 20:57:12 +0900

    update: state 사용해 리스트 작성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
+
+  let [title, func] = useState(['Unity', 'Unreal', 'React']);
+
   return (
     <div className="App">
-      <div class="black-nav">
-        dasdasd
+      <div className='black-nav'>
+        <div style={ {fontSize : '30px' }}>개발 Blog</div>  
+      </div>
+      <div className='list'>
+        <h3>{ title[0] }</h3>
+        <p> 유니티 </p>
+        <hr/>
+        <h3>{ title[1] }</h3>
+        <p> 언리얼 </p>
+        <hr/>
+        <h3>{ title[2] }</h3>
+        <p> 리액트 </p>
+        <hr/>
       </div>
     </div>
   );
