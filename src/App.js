ae883d6995dd6831a450811b2f35d6696125c09f
bello <tkdgns0470@gmail.com>
2024-04-05 20:12:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div class="black-nav">
+        dasdasd
+      </div>
+    </div>
+  );
+}
+
+export default App;
